bacb927ade92c6fb3c374f7d715672a9b44912b9
hasannayeeem <hasannayeeem@gmail.com>
2022-03-26 23:23:34 +0600

    test

---
+++ src/components/Product/Product.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    const {product, handleAddToCart} = props;
+    const {name, img, seller, price, ratings} = product;
+    // const {handleAddTocart} =props;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <p className='product__name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>ratings: {ratings} stars</small></p>
+            </div>
+            <button onClick={()=> handleAddToCart(product)} className='btn-cart'>
+                <p className='btn-text'>Add to Cart</p>
+                
+            </button>
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../images/Logo.svg'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt=""/>
+            <div>
+                <a href="/shop">Order</a>
+                <a href="/orders">Order Review</a>
+                <a href="/inventory">Manage Inventory</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
